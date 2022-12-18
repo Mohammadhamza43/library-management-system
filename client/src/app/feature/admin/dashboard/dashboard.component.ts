a6d0dfbc48114d2efe24c5fdcb793f0d7aa14178
mohammadhamza43 <mohammadhamza43@gmail.com>
2022-12-19 00:58:10 +0500

    feat: implemented dashboard report APIs

--- client/src/app/feature/admin/dashboard/dashboard.component.ts
+++ client/src/app/feature/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {BookService} from "../book/services/book.service";
+import {StudentService} from "../student/service/student.service";
+import {DecodeTokenService} from "../../../shared/services/decode-token.service";
+import {IUser} from "../../../shared/models/IUser";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  totalBooks = 0;
+  totalStudents = 0;
+  totalLibrarians = 0;
+  borrowRequests = 0;
+  user: IUser = {}
+  constructor(
+    private bookService: BookService,
+    private studentService: StudentService,
+    private token: DecodeTokenService
+  ) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.user = this.token.getUser();
+    this.borrowRequests = (await this.bookService.getAllBookRequests().toPromise()).result.length
+    this.totalBooks = (await this.bookService.getAllBooks().toPromise()).result.length
+    this.totalLibrarians = (await this.studentService.getAllUsers().toPromise()).result
+      .filter(x => x.role && x.role.includes('LIBRARIAN')).length
+    this.totalStudents = (await this.studentService.getAllUsers().toPromise()).result
+      .filter(x => x.role && x.role.includes('STUDENT')).length
   }
 
 }
